e035cfff31bffbe5da4a53840cf54dc0936c91bd
Fitzgerald, Mike <mike.fitzgerald@motorolasolutions.com>
2018-01-11 09:56:52 -0600

    Added more not paths

--- mybin/findme.sh
+++ mybin/findme.sh
@@ -18,6 +18,16 @@
    -o -iname "*config*" \
    -o -iname "*.xdc"  \) \
    -not -path "*/.Xil/*" \
+   -not -path "*/.git/*" \
+   -not -path "*/ise/*" \
+   -not -path "*/questa/*" \
+   -not -path "*/modelsim/*" \
+   -not -path "*/vcs/*" \
+   -not -path "*/ip_project/*" \
+   -not -path "*/m3b8_fw/*" \
+   -not -path "*/m2a8_fw/*" \
+   -not -path "*/s4b1_bb_fw/*" \
+   -not -path "*/s5c2_at_dev_fw/*" \
    -not -path "*/.srcs/*" \
    -not -path "*/managed_ip_project/*" \
    | grep -v -P dir-locals |  tee  .project_files.txt
