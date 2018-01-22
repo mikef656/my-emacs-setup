3c4df9d57c43cdb77c6e64aefa33ae23354989f9
Fitzgerald, Mike <mike.fitzgerald@motorolasolutions.com>
2018-01-22 09:02:55 -0600

    Fix bad syntax in Python for Kids file

--- mybin/drawing_polygons.py
+++ mybin/drawing_polygons.py
@@ -5,7 +5,7 @@ from tkinter import *
 tk = Tk()
 canvas = Canvas(tk, width=400, height=400)
 canvas.pack()
-canvas_create_text(150,100,text="There one was a man from Toulouse,")
+canvas.create_text(150,100,text="There one was a man from Toulouse,")
 canvas.create_polygon(10,10,100,10,100,110, fill="",outline="black")
 
 canvas.create_polygon(200,10,240,30,120,100,140,120,fill="",outline="black")
