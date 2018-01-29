8f3d95d8d32b213c64e623126344ef04033e12b7
Fitzgerald, Mike <mike.fitzgerald@motorolasolutions.com>
2018-01-29 08:34:01 -0600

    Add to foo.el to git to use for rectangle example development

---
+++ .emacs.d/my-rectangles/foo.el
@@ -0,0 +1,19 @@
+ aoeu    aoeu something else
+ aoeu    aoeu
+ aoeu    aoeu
+ aoeu    aoeu
+ aoeu    aoeu
+         
+ aoeu aoeu htns
+ aoeu aoeu htns 
+ aoeu aoeu htns  htns
+ aoeu aoeu htns  htns
+ 
+ aoeu aoeu
+
+
+  htns
+  htns
+  htns
+  htns
+  htns
