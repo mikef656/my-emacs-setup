e66855576a46092f197309cff2eec2ee6bfdb2e7
Fitzgerald, Mike <mike.fitzgerald@motorolasolutions.com>
2018-02-16 10:44:54 -0600

    Fixed bug missing less than character in react_to_somethintg.py

--- mybin/react_to_something.py
+++ mybin/react_to_something.py
@@ -8,7 +8,7 @@ canvas.pack()
 canvas.create_polygon(10,10,10,60,50,35)
 def movetriangle(event):
     canvas.move(1,5,0)
-canvas.bind_all('<KeyPress-Return',movetriangle)
+canvas.bind_all('<KeyPress-Return>',movetriangle)
 
 
 
