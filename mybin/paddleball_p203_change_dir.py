a135eafe2341e90d1272a623aef50ee60a8ddfc9
Fitzgerald, Mike <mike.fitzgerald@motorolasolutions.com>
2018-03-15 16:44:20 -0500

    Paddleball

--- mybin/paddleball_p203_change_dir.py
+++ mybin/paddleball_p203_change_dir.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-# based on chapter 13 of Python for Kids 201
-#  "Making the ball bounnce"
+# based on chapter 13 of Python for Kids 203
+#  "Changing the balls direction"
 from tkinter import *
 import random
 import time
@@ -23,6 +23,7 @@ class Ball:
       random.shuffle(starts)
       self.x = starts[0]
       self.y = -3
+      print(" __init__ fn setting x starting position to be",self.x)
       self.canvas_height = self.canvas.winfo_height()
       self.canvas_width  = self.canvas.winfo_width()
 
@@ -30,18 +31,38 @@ class Ball:
     def draw(self):
       self.canvas.move(self.id,self.x,self.y)
       pos = self.canvas.coords(self.id)
+      print("  Ball bottom = ",pos[1])
+      print("  Ball top    = ",pos[3])
+      print("  Ball right  = ",pos[2])
+      print("  Ball left   = ",pos[3])        
       if pos[1] <= 0:
-          self.y = 1
+          self.y = 3
+          print("**setting y =3")
+          print("        x = %d base 10" % self.x)
       if pos[3] >= self.canvas_height:
-          self.y = -1
+          self.y = -3
+          print("**Canvas height = ",self.canvas_height)
+          print("**setting y = -3")
+          print("        x =",self.x)
+          print("        x = %d base 10" % self.x)
+      if pos[0] <= 0:
+          self.x = 3
+          print("**Setting x = 3")
+          print("        y = %d base 10" % self.y)
+      if pos[2] >= self.canvas_width:
+          print("**Canvas width = ",self.canvas_width)
+          self.x = -3
+          print("**Setting x = -3")
+          print("        y = %d base 10" % self.y)
     
 ball = Ball(canvas, 'red')
 
 while 1:
     ball.draw()
+    print("------------------ drew the ball -------------------------")
     tk.update_idletasks()
     tk.update()
-    time.sleep(0.01)
+    time.sleep(0.1)
 
 
 
