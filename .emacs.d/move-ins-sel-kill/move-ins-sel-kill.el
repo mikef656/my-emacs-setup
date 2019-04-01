1c95e85ddb3d26ae97372e79b58ae50658b20d6f
Fitzgerald, Mike <mike.fitzgerald@motorolasolutions.com>
2019-04-01 13:49:24 -0500

    Add defun xah-beginning-of-line-or-block

--- .emacs.d/move-ins-sel-kill/move-ins-sel-kill.el
+++ .emacs.d/move-ins-sel-kill/move-ins-sel-kill.el
@@ -300,7 +300,49 @@ If point was already at that position, move point to the other"
                     (back-to-indentation)
                     (and (= oldpos (point))
                          (beginning-of-line))))))))))
-;
+
+;; -----------------------------------------------------------------------------
+;; Give this a try
+;; -----------------------------------------------------------------------------
+(defun xah-beginning-of-line-or-block ()
+  "Move cursor to beginning of line or previous paragraph.
+
+• When called first time, move cursor to beginning of char in current line. (if already, move to beginning of line.)
+• When called again, move cursor backward by jumping over any sequence of whitespaces containing 2 blank lines.
+
+URL `http://ergoemacs.org/emacs/emacs_keybinding_design_beginning-of-line-or-block.html'
+Version 2017-05-13"
+  (interactive)
+  (let (($p (point)))
+    (if (or (equal (point) (line-beginning-position))
+            (equal last-command this-command ))
+        (if (re-search-backward "\n[\t\n ]*\n+" nil "NOERROR")
+            (progn
+              (skip-chars-backward "\n\t ")
+              (forward-char ))
+          (goto-char (point-min)))
+      (progn
+        (back-to-indentation)
+        (when (eq $p (point))
+          (beginning-of-line))))))
+          
+; -----------------------------------------------------------------------------
+; give this a try
+; -----------------------------------------------------------------------------
+(defun xah-end-of-line-or-block ()
+  "Move cursor to end of line or next paragraph.
+
+• When called first time, move cursor to end of line.
+• When called again, move cursor forward by jumping over any sequence of whitespaces containing 2 blank lines.
+
+URL `http://ergoemacs.org/emacs/emacs_keybinding_design_beginning-of-line-or-block.html'
+Version 2017-05-30"
+  (interactive)
+  (if (or (equal (point) (line-end-position))
+          (equal last-command this-command ))
+      (progn
+        (re-search-forward "\n[\t\n ]*\n+" nil "NOERROR" ))
+    (end-of-line)))
 
 (defun smart-end-of-line ()
  "Move point to first non-whitespace character or beginning-of-line.
