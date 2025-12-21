4943f1f84e689ba317d1cbe4b7dd54b9e069c5c6
mikef656 <mikef656@gmail.com>
2025-12-21 00:03:52 -0700

    trying out vertico

---
+++ .emacs.d/elpa/consult-20250509.2/consult-pkg.el
@@ -0,0 +1,10 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "consult" "20250509.2"
+  "Consulting completing-read."
+  '((emacs  "28.1")
+    (compat "30"))
+  :url "https://github.com/minad/consult"
+  :commit "1d10b084401335c87cc3e92de683aef9a0746aac"
+  :revdesc "1d10b0844013"
+  :keywords '("matching" "files" "completion")
+  :maintainers '(("Daniel Mendler" . "mail@daniel-mendler.de")))
